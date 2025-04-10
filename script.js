2dae73986a27e1e4d56880a8d62e7596d8951e91
Antonio Wang <antoniowang611@gmail.com>
2025-04-10 16:57:00 +1000

    aaa

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+import * as THREE from 'https://unpkg.com/three@0.159.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.159.0/examples/jsm/controls/OrbitControls.js';
+/*
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';*/
 
 // manually canvas for precise control 
 document.body.style.margin = '0';
